f3e7da2be78f11620dd7588fda5fb72808539bc1
Denys Podobrii <denys.podobrii@gmail.com>
2025-09-04 21:18:40 +0300

    add welcome page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
+import Image from "next/image";
+import {Button} from "@heroui/button";
+import styles from './page.module.css'
+
 export default function Home() {
-  return (
-    <div>
-        Welcome Page
-    </div>
-  );
+    return (
+      <div className={styles.container}>
+          <div className='flex justify-center items-center w-[400px] h-full mx-auto flex-col'>
+              <h1 className='text-6xl font-bold'>Welcome to</h1>
+              <Image src='/moviehub_logo.svg' alt='MovieHub Logo' width={400} height={300}/>
+              <div className='flex justify-center gap-8 w-full mt-4'>
+                  <Button color='primary' variant='ghost'>Log In</Button>
+                  <Button color='primary' variant='flat'>Continue as Guest</Button>
+              </div>
+          </div>
+      </div>
+
+    );
 }
