5f706e4fffacb7eb78e992c45e65fa24d522301e
Denys Podobrii <denys.podobrii@gmail.com>
2025-10-01 23:45:30 +0300

    bug fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,8 +3,11 @@ import {NextRequest, NextResponse} from "next/server";
 export function middleware(req: NextRequest) {
     const loginType = req.cookies.get("loginType")?.value;
 
+    console.log("Middleware - loginType:", loginType);
+    console.log("Middleware - pathname:", req.nextUrl.pathname);
+
     if (!loginType && req.nextUrl.pathname.startsWith("/movies")) {
-        return Response.redirect(new URL("/login", req.url));
+        return NextResponse.redirect(new URL("/login", req.url));
     }
 
     return NextResponse.next();
--- src/utils/getPosterUrl.ts
+++ src/utils/getPosterUrl.ts
@@ -1,9 +1,3 @@
 export const getPosterUrl = (url: string, type: string): string => {
-    const movieImageBaseUrl = process.env.MOVIE_IMAGE_URL;
-
-    if (!movieImageBaseUrl) {
-        throw new Error("MOVIE_IMAGE_URL is not defined in environment variables");
-    }
-
-    return movieImageBaseUrl + "/" + type + url;
+    return url ? process.env.MOVIE_IMAGE_URL! + "/" + type + url : "";
 }
