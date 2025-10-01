8541187029907ff35f9006ed89fbf687214b9189
Denys Podobrii <denys.podobrii@gmail.com>
2025-10-02 00:06:03 +0300

    change secure for cookies

--- src/actions/auth.ts
+++ src/actions/auth.ts
@@ -9,8 +9,8 @@ export async function loginUserAction( username: string, password: string) {
 
     const user = await userService.userLogin(username, password);
 
-    cookieStore.set("loginType", "user", {httpOnly: true, maxAge: 60 * 60, path: "/"});
-    cookieStore.set("userId", JSON.stringify(user.id), {httpOnly: true, maxAge: 60 * 60, path: "/"});
+    cookieStore.set("loginType", "user", {httpOnly: true, maxAge: 60 * 60, path: "/", secure: process.env.NODE_ENV === "production"});
+    cookieStore.set("userId", JSON.stringify(user.id), {httpOnly: true, maxAge: 60 * 60, path: "/", secure: process.env.NODE_ENV === "production"});
     redirect("/movies");
 }
 
@@ -18,13 +18,13 @@ export async function logoutUserAction() {
     const cookieStore = await cookies()
     cookieStore.delete("userId")
     cookieStore.delete("loginType")
-    redirect("/login");
+    redirect("/");
 }
 
 export async function loginGuestAction() {
     const cookieStore = await cookies();
 
-    cookieStore.set("userId", "", { httpOnly: true, maxAge: 0, path: "/" })
-    cookieStore.set("loginType", "guest", {httpOnly: true, maxAge: 60 * 60, path: "/", secure: process.env.NODE_ENV === "production",})
+    cookieStore.delete('user');
+    cookieStore.set("loginType", "guest", {httpOnly: true, maxAge: 60 * 60, path: "/", secure: process.env.NODE_ENV === "production",});
     redirect("/movies");
 }
